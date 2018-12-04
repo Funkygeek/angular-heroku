5e30658ed2903ff791dd7bd3914d2a0d81997436
Robert Leidl <Funkygeek@users.noreply.github.com>
2018-12-04 12:05:25 +1100

    Update heroku_prep.js
    Removed App name

--- heroku_prep.js
+++ heroku_prep.js
@@ -27,11 +27,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files from the dist directory
-app.use(express.static(__dirname + '/dist/${app_name}'));
+app.use(express.static(__dirname + '/dist));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'/dist/${app_name}/index.html'));
+res.sendFile(path.join(__dirname+'/dist/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
