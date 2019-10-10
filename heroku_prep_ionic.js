2a9ed9468dc8835db3341df2a93adf38b3f93270
Robert Leidl <Funkygeek@users.noreply.github.com>
2019-10-10 15:05:10 +1100

    Update heroku_prep_ionic.js

--- heroku_prep_ionic.js
+++ heroku_prep_ionic.js
@@ -22,19 +22,10 @@ const app_name = package_file.name;
 const server_template = `
 const express = require('express');
 const bodyParser = require('body-parser');  
-const cors = require('cors');
 const app = express();
 
 app.use(bodyParser.urlencoded({'extended':'true'}));            
 app.use(bodyParser.json());                                     
-app.use(cors());
-
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header('Access-Control-Allow-Methods', 'DELETE, PUT');
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
 
 app.use(express.static('www'));
 
